1d650b81b2824a1302eae47b7b93b6ea415fcd47
Daniel Kvist <daniel@danielkvist.net>
2011-09-11 11:29:19 +0200

    Inför lab 3

--- lab2/lab2/mom.c
+++ lab2/lab2/mom.c
@@ -1,86 +1,98 @@
 /************** ML13 STATUS REGISTER READ **************\
-ï»¿Bit 7, 		 CLOSING.			  1 = The door is closing.      
-Bit 6, 		 OPENING. 			  1 = Door is opening             
-Bit 5,       S2: Sense 2.         1 = Door is closed                
-Bit 4,       S4: Sense 4.         1 = passed  Sensor 4 and is nearly closed     
-Bit 3,       S1: Sense 1.         1 = Door is wide open.        
-Bit 2,       S3: Sense 3.         1 = passed  Sensor 3 and is nearly open.      
-Bit 1,       RIGHT: Sensor B. 	  1 = Sensor B is activated (Button S2) 
-Bit 0,       LEFT: Sensor A.      1 = Sensor A is activated (Button S1) 
+Bit 7, 		CLOSING.	  	1 = The door is closing.      
+Bit 6, 		OPENING. 	  	1 = Door is opening             
+Bit 5,       	S2: Sense 2.         	1 = Door is closed                
+Bit 4,       	S4: Sense 4.         	1 = passed  Sensor 4 and is nearly closed     
+Bit 3,       	S1: Sense 1.         	1 = Door is wide open.        
+Bit 2,       	S3: Sense 3.         	1 = passed  Sensor 3 and is nearly open.      
+Bit 1,       	RIGHT: Sensor B. 	1 = Sensor B is activated (Button S2) 
+Bit 0,       	LEFT: Sensor A.      	1 = Sensor A is activated (Button S1) 
 /*******************************************************\
 * ***************************************************** *
 /************ ML13 CONTROL REGISTER WRITE **************\
-ï»¿Bit 7-2,     Not used.               
-Bit 1,       CLOSE   			  1 = Actuate "Close Door" 
-Bit 0,       OPEN     			  1 = Actuate "Open Door" 
+Bit 7-2,     	Not used.               
+Bit 1,       	CLOSE   		1 = Actuate "Close Door" 
+Bit 0,       	OPEN     		1 = Actuate "Open Door" 
 
 Setting bit 1 AND bit 0 to 1 will yield an undefined behavior.
+/*******************************************************
+* ***************************************************** *
+/****** ML13 INTERUPT STATUS REGISTER READ BASE+1 ******\
+Bit 5 to 0 reflects any pending interrupt request. NOTE: These are set during RESET and cleared when writing the Interrupt Control register (se below). 
+Bit 7-6,   Not Used.              
+Bit 5,       ICLOSING: Is Closing	1 = Door is moving towards closed position.
+Bit 4,       IOPENING: Is Opening     	1 = Door is moving towards open position.
+Bit 3,       SLEFT: Sensor A.           1 = Sensor A is activated (Button S1) 
+Bit 2,       RIGHT: Sensor B. 		1 = Sensor B is activated (Button S2)
+Bit 1,       ISENS2: Sensor 2           1 = Door is closed                
+Bit 0,       ISENS1: Sensor 1.          1 = Door is open.
 /*******************************************************/
 
 // I/O
-#define ML13Status 		0xB00
+#define ML13Status 	0xB00
 #define ML13Control 	0xB00
+// Lägg till IRQSTATUS/IRWCONTROL 0xB01
 #define readControl 	*((char *)ML13Status)
 #define setControl(x) 	*((char *)ML13Control) = x
 
 // MASKS
-#define OPEN_DOOR 		0x01
-#define CLOSE_DOOR 		0x02
+#define OPEN_DOOR 	0x01
+#define CLOSE_DOOR 	0x02
 #define DOOR_OPENED 	0x08 	// 0000 1000 Bit 3
 #define DOOR_CLOSED 	0x32 	// 0010 0000 Bit 5
-#define SENSOR 			0x03	// 0000 0011 Bit 1 och 2
-#define TIME_FOR_DELAY 	0xFFFF	// Time for delay, needs to be huge!
+#define SENSOR 		0x03	// 0000 0011 Bit 1 och 2
+#define TIME_FOR_DELAY 	0xFF	// Time for delay, needs to be huge!
 
 int main(int argc, char *argv[])
 {
-	// 1. Initiera (GÃ¶rs ovan med definitionerna?)
+	// 1. Initiera (Görs ovan med definitionerna?)
 	unsigned int i, attemptClose, doorUnlocked;
-	// SÃ¤tter doorUnlocked till 1 eftersom den inte verka anvÃ¤ndas, har Ã¤ven
-	// kommenterat ut lÃ¥s-kod nedan
+	// Sätter doorUnlocked till 1 eftersom den inte verka användas, har även
+	// kommenterat ut lås-kod nedan
 	doorUnlocked = 1;
 	while(1)
 	{
-		// Om dÃ¶rren Ã¤r olÃ¥st (det Ã¤r den alltid!)
+		// Om dörren är olåst (det är den alltid!)
 		if(doorUnlocked)
 		{
-			// DÃ¶rren ska inte lÃ¥sas, fÃ¶rsÃ¶k Ã¶ppna den som vi fÃ¥r utslag
+			// Dörren ska inte låsas, försök öppna den som vi får utslag
 			if(readControl & SENSOR)
 			{
-				// Ã–ppna dÃ¶rren
+				// Öppna dörren
 				setControl(OPEN_DOOR);
 				
-				// VÃ¤nta pÃ¥ att dÃ¶rren Ã¶ppnats
+				// Vänta på att dörren öppnats
 				while(!(readControl & DOOR_OPENED));
 				
-				// SÃ¤tt fÃ¶rsÃ¶ksflaggan
+				// Sätt försöksflaggan
 				attemptClose = 1;
 				while(attemptClose)
 				{
-					// VÃ¤nta ett tag, stÃ¤ng sedan dÃ¶rren
+					// Vänta ett tag, stäng sedan dörren
 					for(i = 0; i < TIME_FOR_DELAY; i++)
 					{
-						// Har vi fÃ¥tt nytt utslag?
+						// Har vi fått nytt utslag?
 						if(readControl & SENSOR)
 						{
-							// NollstÃ¤ll dÃ¥ rÃ¤knaren fÃ¶r att bÃ¶rja rÃ¤kna om
+							// Nollställ då räknaren för att börja räkna om
 							i = 0;
 						}
 					}
 					
-					// Tiden Ã¤r ute, pÃ¥bÃ¶rja stÃ¤ngning av dÃ¶rren
+					// Tiden är ute, påbörja stängning av dörren
 					attemptClose = 0;
 					setControl(CLOSE_DOOR);
 					
-					// Kolla sÃ¥ att ingen vill Ã¶ppna dÃ¶rren under stÃ¤ngning
+					// Kolla så att ingen vill öppna dörren under stängning
 					while(!(readControl & DOOR_CLOSED))
 					{
-						// Har vi nytt utslag under stÃ¤ngning?
+						// Har vi nytt utslag under stängning?
 						if(readControl & SENSOR)
 						{
-							// Ã–ppna dÃ¥ dÃ¶rren
+							// Öppna då dörren
 							setControl(OPEN_DOOR);
 							
-							// SÃ¤tt stÃ¤ngningsflaggan igen fÃ¶r att bÃ¶rja om
+							// Sätt stängningsflaggan igen för att börja om
 							attemptClose = 1;
 							break;
 						}
---
+++ lab3/mom.c
@@ -0,0 +1,106 @@
+/************** ML13 STATUS REGISTER READ **************\
+Bit 7, 		CLOSING.	  	1 = The door is closing.      
+Bit 6, 		OPENING. 	  	1 = Door is opening             
+Bit 5,       	S2: Sense 2.         	1 = Door is closed                
+Bit 4,       	S4: Sense 4.         	1 = passed  Sensor 4 and is nearly closed     
+Bit 3,       	S1: Sense 1.         	1 = Door is wide open.        
+Bit 2,       	S3: Sense 3.         	1 = passed  Sensor 3 and is nearly open.      
+Bit 1,       	RIGHT: Sensor B. 	1 = Sensor B is activated (Button S2) 
+Bit 0,       	LEFT: Sensor A.      	1 = Sensor A is activated (Button S1) 
+/*******************************************************\
+* ***************************************************** *
+/************ ML13 CONTROL REGISTER WRITE **************\
+Bit 7-2,     	Not used.               
+Bit 1,       	CLOSE   		1 = Actuate "Close Door" 
+Bit 0,       	OPEN     		1 = Actuate "Open Door" 
+
+Setting bit 1 AND bit 0 to 1 will yield an undefined behavior.
+/*******************************************************
+* ***************************************************** *
+/****** ML13 INTERUPT STATUS REGISTER READ BASE+1 ******\
+Bit 5 to 0 reflects any pending interrupt request. NOTE: These are set during
+RESET and cleared when writing the Interrupt Control register (se below).
+ 
+Bit 7-6,   	Not Used.              
+Bit 5,       	ICLOSING: Is Closing	1 = Door is moving towards closed position.
+Bit 4,       	IOPENING: Is Opening    1 = Door is moving towards open position.
+Bit 3,       	SLEFT: Sensor A.        1 = Sensor A is activated (Button S1) 
+Bit 2,       	RIGHT: Sensor B. 	1 = Sensor B is activated (Button S2)
+Bit 1,       	ISENS2: Sensor 2        1 = Door is closed                
+Bit 0,       	ISENS1: Sensor 1.       1 = Door is open.
+/*******************************************************/
+
+// I/O
+#define ML13Status 	0xB01
+#define ML13Control 	0xB01
+// Lägg till IRQSTATUS/IRWCONTROL 0xB01
+#define readControl 	*((char *)ML13Status)
+#define setControl(x) 	*((char *)ML13Control) = x
+
+// IRQ TYPES
+#define NO_IRQ_TYPE = 0;
+
+// MASKS
+#define OPEN_DOOR 	0x01
+#define CLOSE_DOOR 	0x02
+#define DOOR_OPENED 	0x04 	// 0000 0100 Bit 3
+#define DOOR_CLOSED 	0x02 	// 0010 0000 Bit 5
+#define SENSOR 		0x01	// 0000 0011 Bit 1 och 2
+#define TIME_FOR_DELAY 	0xFF	// Time for delay
+#define TIME_OUT	0x20	// Timeout is given
+
+int interrupt_type;
+//int standby();
+
+/**
+
+TODO
+
+
+*/
+
+int main()
+{
+	// 1. Initiera (Görs ovan med definitionerna?)
+	unsigned int door = DOOR_CLOSED;
+	init_irq();
+
+	while(1)
+	{
+		// Sätt IRQ typ till 0 eftersom vi inte har avbrott
+		interrupt_type = NO_IRQ_TYPE;
+		
+		// Vänta på avbrott, WAI i assembler modulen
+		standby();
+		
+		// Nu har vi avbrott, switcha på interrupt_type som sätts
+		// i assemblermodulen för att avgöra vilken typ av avbrott
+		switch(interrupt_type)
+		{
+			// Sensor har gett utslag, öppna dörren
+			case SENSOR:
+				if(door == DOOR_CLOSED)
+					setControl(OPEN_DOOR);
+				break;
+			// Dörren har stängts
+			case DOOR_CLOSED:
+				door = DOOR_CLOSED;
+				break;
+			// Dörren har öppnats, börja räkna ner innan den ska stängas igen
+			case DOOR_OPENED:
+				door = DOOR_OPENED;
+				set_timeout(30);
+				break;
+			// Timeouten är klar, stäng nu dörren
+			case TIME_OUT:
+				if(door == DOOR_OPENED)
+					setControl(CLOSE_DOOR);
+				break;
+		}
+	}
+	return 0;
+}
+
+
+
+
